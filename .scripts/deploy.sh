7f472a6626204ab27353303f0e955e0093256515
DevRajib <mnhodarajib@gmail.com>
2024-08-30 13:40:17 +0600

    f

---
+++ .scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+echo "Creating Production Build..."
+npm run build
+
+echo "Deployment Finished!"
